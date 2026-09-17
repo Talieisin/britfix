9a7eb900a69a05479c1ebb4dc18a59049090815c
George Elphick <george.elphick@talieisin.co.uk>
2026-09-17 08:33:05 +0100

    Also isolate HOME in the test config fixture

--- conftest.py
+++ conftest.py
@@ -11,5 +11,6 @@ def isolated_user_config(monkeypatch, tmp_path):
     home = tmp_path / 'isolated-user-home'
     home.mkdir()
     monkeypatch.setattr(Path, 'home', classmethod(lambda cls: home))
+    monkeypatch.setenv('HOME', str(home))
     monkeypatch.setenv('XDG_CONFIG_HOME', str(home / '.config'))
     monkeypatch.setenv('APPDATA', str(home / 'AppData'))
